e63b88310ec07462d8b2cb0abb8ea59b9f7edd0d
Maxim Pokrovsky <maxim.pokrovsky@outlook.com>
2022-03-12 19:28:39 +0300

    Написал логику сортировки файлов

---
+++ FileSorter.BL/Controller/FileSorterController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace FileSorter.BL.Controller
+{
+    public class FileSorterController
+    {
+        private string _pathOriginDirectory;
+        private string[] _files;
+        private string _afterSortDirectionName = "AfterSort";
+
+        public FileSorterController(string pathOriginDirectory)
+        {
+            if (string.IsNullOrWhiteSpace(pathOriginDirectory))
+            {
+                throw new ArgumentNullException("Путь к директории не может быть пустым или NULL.", nameof(pathOriginDirectory));
+            }
+
+            if (Directory.Exists(pathOriginDirectory))
+            {
+                _pathOriginDirectory = pathOriginDirectory;
+            }
+            else
+            {
+                throw new ArgumentException("Путь к директории не найден.");
+            }
+
+            _files = GetFilesFromDirectory(_pathOriginDirectory);
+
+            Directory.CreateDirectory(_pathOriginDirectory + "\\" + _afterSortDirectionName);
+
+        }
+
+        private string[] GetFilesFromDirectory(string path)
+        {
+            List<string> files = new List<string>(Directory.GetFiles(path));
+            string[] directories = Directory.GetDirectories(path);
+
+            foreach (string directory in directories)
+            {
+                files.AddRange(GetFilesFromDirectory(directory));
+            }
+
+            if (files.Count > 0)
+            {
+                return files.ToArray();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public void Sort()
+        {
+            for (int i = 0; i < _files.Length; i++)
+            {
+                string targetFile = _files[i];
+                string fileName = Path.GetFileName(targetFile);
+                string targetDirectory = GetDateFileCreation(targetFile);
+                string newPath = _pathOriginDirectory + "\\" + _afterSortDirectionName + "\\" + targetDirectory;
+
+                if (Directory.Exists(newPath) == false)
+                {
+                    Directory.CreateDirectory(newPath);
+                }
+
+                File.Move(targetFile, newPath + "\\" + fileName);
+            }
+        }
+
+        private string GetDateFileCreation(string filePath)
+        {
+            DateTime dateCreation = File.GetCreationTime(filePath);
+            DateTime dateLastWrite = File.GetLastWriteTime(filePath);
+
+            if (dateCreation < dateLastWrite)
+            {
+                return dateCreation.ToShortDateString().ToString();
+            }
+            else
+            {
+                return dateLastWrite.ToShortDateString().ToString();
+            }
+        }
+    }
+}
